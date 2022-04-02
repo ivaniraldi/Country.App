2430300927a90e3b85f670b7b09968df623f7b5b
ivaniraldi <iraldiban@gmail.com>
2022-04-02 19:53:19 -0300

    Deploy second try

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ conn.sync({ force: true }).then(() => {
                 })
             });
         })
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
