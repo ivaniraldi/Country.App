ce00373cd7afdfcb746878e3af602845f6cdb4cb
ivaniraldi <iraldiban@gmail.com>
2022-04-02 21:20:05 -0300

    Third try to deploy

--- api/index.js
+++ api/index.js
@@ -19,28 +19,14 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn, Country } = require('./src/db.js');
-const { default: axios } = require("axios")
+const { default: axios } = require("axios");
+const getCountries1 = require('./src/controllers/loader.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  axios.get("https://restcountries.com/v3/all")
-        .then(res => {
-            return res.data.forEach(c => {
-                Country.findOrCreate({ 
-                    where: {
-                        id: c.cca3 || "No se encontró una id existente.",
-                        name: c.name.common || "No se encontró ese país.",
-                        flags: c.flags.find(e => e.includes(".svg")) || "No se encontró una imagen de la bandera.",
-                        continent: c.region || "No se encontró relación con ningún continente.",
-                        subregion: c.subregion || "No se encontró una subregión.",
-                        capital: c.capital && c.capital[0] || "No se encontró una capital.",
-                        area: c.area || 0,
-                        population: c.population || 0
-                    }
-                })
-            });
-        })
-  server.listen(process.env.PORT, () => {
+conn.sync({ force: false }).then(() => {
+  getCountries1(Country)
+  const port = process.env.PORT || 3001
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
---
+++ api/src/controllers/loader.js
@@ -0,0 +1,40 @@
+const { default: axios } = require("axios")
+
+
+const getCountries1 = async (Country) => {
+  try {
+    const apiResponse = await axios.get("https://restcountries.com/v3/all")
+    const apiCountries = apiResponse.data
+
+    for (let i = 0; i < apiCountries.length; i++) {
+      const country = apiCountries[i]
+      const addCountry = {
+        id: country.cca3 || "No se encontró una id existente.",
+        name: country.name.common || "No se encontró ese país.",
+        flags: country.flags.find(e => e.includes(".svg")) || "No se encontró una imagen de la bandera.",
+        continent: country.region || "No se encontró relación con ningún continente.",
+        subregion: country.subregion || "No se encontró una subregión.",
+        capital: country.capital && country.capital[0] || "No se encontró una capital.",
+        area: country.area || 0,
+        population: country.population || 0
+      }
+      try {
+        Country.findOrCreate({
+          where: {
+            name: addCountry.name
+          },
+          defaults: addCountry
+        })
+
+        // console.log('▄'.green, addCountry.name, 'Cargado correctamente'.green)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    console.log(`${apiCountries.length} paises cargados correctamente`)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+module.exports = getCountries1
