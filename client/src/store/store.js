9aa3ec1e4bfcf2aa4bfa7cb6d664376069858c78
ivaniraldi <iraldiban@gmail.com>
2024-03-05 18:12:26 -0300

    really, this time is fixed store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducer/reducer";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 export const store = createStore (
