79ea73bb99b372ef3a3791b301649cc938c64d17
ivaniraldi <iraldiban@gmail.com>
2024-03-05 18:09:16 -0300

    changed store w/out extension

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,8 @@
-import { createStore, applyMiddleware } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension"
-import {thunk} from "redux-thunk"
-import rootReducer from "../reducer/reducer.js"
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducer";
+import thunk from "redux-thunk";
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export const store = createStore (
+    rootReducer, 
+    composeEnhancer(applyMiddleware(thunk)))
